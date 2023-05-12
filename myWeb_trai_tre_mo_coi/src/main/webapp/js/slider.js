e223c56014271713e7a94cfa74cfb583446962fd
NgoMauTruongQB <truongngo2707@gmail.com>
2023-05-13 01:27:12 +0700

    reponsive user page

--- myWeb_trai_tre_mo_coi/src/main/webapp/js/slider.js
+++ myWeb_trai_tre_mo_coi/src/main/webapp/js/slider.js
@@ -1,23 +1,31 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous 
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+
+  // Tăng giá trị slideIndex sau mỗi 3 giây
+  setTimeout(function() {
+    slideIndex++;
+    showSlides(slideIndex);
+  }, 3000);
 }
